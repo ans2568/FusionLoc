e1be989476fd56b932bb83014f24f500c1871bbf
ans2568 <ans2568@gmail.com>
2023-09-10 06:57:20 +0000

    Argument Parser 추가 및 사용법 추가

---
+++ ros/README.md
@@ -0,0 +1,24 @@
+# ROS bag 파일로 데이터 셋 만들기
+
+### ROS bag 파일 내부에 있어야 할 토픽 타입
+- 이미지 토픽
+  - default : /image_raw/compressed
+  - 현재 sensor_msgs/msg/CompressedImage 타입만 지원
+  - 추후 sensor_msgs/msg/Image 지원 예정
+- 라이다 토픽
+  - default : /scan
+  - 현재 sensor_msgs/msg/LaserScan 타입만 지원
+  - 추후 sensor_msgs/msg/PointCloud2 지원 예정
+- 위치 토픽(nav_msgs/msg/Odometry)
+  - default : /odom
+
+### 데이터 셋 만드는 법
+```
+python bag2csv.py --bag /path/to/ros2_ws/bag/data.bag \ 
+--csvDir ../data/custom_dataset/csv \
+--lidarDir ../data/custom_dataset/lidar \
+--cameraDir ../data/custom_dataset/camera \
+--cameraTopic /compressed/image_raw \
+--lidarTopic /scan \
+--odometryTopic /odom
+```
--- ros/bag2csv.py
+++ ros/bag2csv.py
@@ -2,7 +2,6 @@ import os
 import csv
 import argparse
 
-import math
 import cv2
 import numpy as np
 import open3d as o3d
@@ -12,86 +11,109 @@ from rclpy.serialization import deserialize_message
 from rosidl_runtime_py.utilities import get_message
 from rosbag2_py import SequentialReader, StorageOptions, ConverterOptions, StorageFilter
 
-bag_file = '/path/to/ros2_ws/bag/data.bag'
-csv_dir = '../data/gazebo_dataset/csv'
-lidar_dir = '../data/gazebo_dataset/lidar'
-camera_dir = '../data/gazebo_datasetcamera'
 
-if not os.path.exists(csv_dir):
-    os.makedirs(csv_dir)
-if not os.path.exists(lidar_dir):
-    os.makedirs(lidar_dir)
-if not os.path.exists(camera_dir):
-    os.makedirs(camera_dir)
+parser = argparse.ArgumentParser(description='Converting a ROS bag file to a CSV file')
+parser.add_argument('--bag', type=str, default='/path/to/ros2_ws/bag/data.bag', help='ROS bag file')
+parser.add_argument('--csvDir', type=str, default='../data/jackal_dataset/csv', help='The directory to save CSV file')
+parser.add_argument('--lidarDir', type=str, default='../data/jackal_dataset/lidar', help='The directory to save pcd file')
+parser.add_argument('--cameraDir', type=str, default='../data/jackal_dataset/camera', help='The directory to save png file')
+parser.add_argument('--cameraTopic', type=str, default='/image_raw/compressed', help='Camera topic name in rosbag')
+parser.add_argument('--lidarTopic', type=str, default='/scan', help='PointCloud topic name in rosbag')
+parser.add_argument('--odometryTopic', type=str, default='/odom', help='Odometry topic name in rosbag')
 
-storage_options = StorageOptions(uri=bag_file, storage_id="sqlite3")
-converter_options = ConverterOptions('cdr', 'cdr')
-storage_filter = StorageFilter(topics=['/scan', '/camera/image_raw/compressed', '/tf'])
+if __name__ == "__main__":
+    opt = parser.parse_args()
 
-# open the bag file
-reader = SequentialReader()
-reader.open(storage_options, converter_options)
-reader.set_filter(storage_filter)
-lidar_topic = '/scan'
-camera_topic = '/camera/image_raw/compressed'
-tf_topic = '/tf'
+    csv_dir = opt.csvDir
+    lidar_dir = opt.lidarDir
+    camera_dir = opt.cameraDir
 
-lidar_time = None
-camera_time = None
-tf_time = None
+    lidar_topic = opt.lidarTopic
+    camera_topic = opt.cameraTopic
+    odometry_topic = opt.odometryTopic
 
-csv_file = open(os.path.join(csv_dir, "whole_data.csv"), mode="w")
-csv_writer = csv.writer(csv_file)
-csv_writer.writerow(["time", "encoder_L", "encoder_R", "angular_vel_x", "angular_vel_y", "angular_vel_z", "linear_acc_x", "linear_acc_y", "linear_acc_z", "pose_x","pose_y", "pose_theta", "GT_pose_x", "GT_pose_y", "GT_pose_theta", "GPS_lon", "GPS_lat", "gt", "image_path", "lidar_path"])
+    if not os.path.exists(csv_dir):
+        os.makedirs(csv_dir)
+    if not os.path.exists(lidar_dir):
+        os.makedirs(lidar_dir)
+    if not os.path.exists(camera_dir):
+        os.makedirs(camera_dir)
 
-while reader.has_next():
-    topic, data, timestamp = reader.read_next()
-    timestamp = datetime.fromtimestamp(timestamp/ 1e9)
-    millisecond = str(timestamp.timestamp()).split('.')[1]
-    sec = "{0}".format(str(timestamp.year)+str(timestamp.month)+str(timestamp.day)+"_"+str(timestamp.hour)+str(timestamp.minute).zfill(2)+str(timestamp.second).zfill(2))
-    if topic == lidar_topic:
-        type = get_message('sensor_msgs/msg/LaserScan')
-        msg = deserialize_message(data, type)
-        if not lidar_time:
-            lidar_time = timestamp
-        if int(timestamp.timestamp() - lidar_time.timestamp()) >= 1:
-            file_name = "{0}.pcd".format(sec)
-            angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
-            xs = np.array(msg.ranges) * np.cos(angles)
-            ys = np.array(msg.ranges) * np.sin(angles)
-            zs = np.zeros_like(np.array(msg.ranges))
-            points = np.column_stack((xs, ys, zs))
-            pcd = o3d.geometry.PointCloud()
-            pcd.points = o3d.utility.Vector3dVector(points)
-            o3d.io.write_point_cloud(os.path.join(lidar_dir, file_name), pcd)
-            csv_writer.writerow([sec, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", os.path.join(lidar_dir, file_name)])
-            lidar_time = timestamp
+    storage_options = StorageOptions(uri=opt.bag, storage_id="sqlite3")
+    converter_options = ConverterOptions('cdr', 'cdr')
+    storage_filter = StorageFilter(topics=[lidar_topic, camera_topic, odometry_topic])
 
-    elif topic == camera_topic:
-        type = get_message('sensor_msgs/msg/CompressedImage')
-        msg = deserialize_message(data, type)
-        if not camera_time:
-            camera_time = timestamp
-        if int(timestamp.timestamp() - camera_time.timestamp()) >= 1:
-            file_name = "{0}.png".format(sec)
-            np_arr = np.frombuffer(msg.data, np.uint8)
-            image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-            cv2.imwrite(os.path.join(camera_dir, file_name), image_np)
-            csv_writer.writerow([sec, "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", os.path.join(camera_dir, file_name), ""])
-            camera_time = timestamp
+    # open the bag file
+    reader = SequentialReader()
+    reader.open(storage_options, converter_options)
+    reader.set_filter(storage_filter)
 
-    elif topic == tf_topic:
-        type = get_message('tf2_msgs/msg/TFMessage')
-        msg = deserialize_message(data, type)
-        if not tf_time:
-            tf_time = timestamp
-        if int(timestamp.timestamp() - tf_time.timestamp()) >= 1:
-            for transform in msg.transforms:
-                if transform.child_frame_id == "base_footprint" and transform.header.frame_id == "odom":
-                    pose_x = transform.transform.translation.x
-                    pose_y = transform.transform.translation.y
-                    quaternion = transform.transform.rotation
-                    theta = math.atan2(2 * (quaternion.w * quaternion.z + quaternion.x * quaternion.y), 
-                                    1 - 2 * (quaternion.y**2 + quaternion.z**2))
-                    csv_writer.writerow([sec, "", "", "", "", "", "", "", "", "", "", "", pose_x, pose_y, theta, "", "", "", "", ""])
-                    tf_time = timestamp
+    lidar_time = None
+    camera_time = None
+    odom_time = None
+
+    csv_file = open(os.path.join(csv_dir, "whole_data.csv"), mode="w")
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["time", "x", "y", "z", "qw","qx", "qy", "qz", "image_path", "lidar_path"])
+
+    while reader.has_next():
+        topic, data, timestamp = reader.read_next()
+        timestamp = datetime.fromtimestamp(timestamp/ 1e9)
+        millisecond = str(timestamp.timestamp()).split('.')[1]
+        sec = "{0}".format(str(timestamp.year)+str(timestamp.month)+str(timestamp.day)+"_"+str(timestamp.hour)+str(timestamp.minute).zfill(2)+str(timestamp.second).zfill(2))
+        if topic == lidar_topic:
+            type = get_message('sensor_msgs/msg/LaserScan')
+            msg = deserialize_message(data, type)
+            if not lidar_time:
+                lidar_time = timestamp
+            if int(timestamp.timestamp() - lidar_time.timestamp()) >= 1:
+                file_name = "{0}.pcd".format(sec)
+                angles = np.arange(msg.angle_min, msg.angle_max, msg.angle_increment)
+                xs = np.array(msg.ranges) * np.cos(angles)
+                ys = np.array(msg.ranges) * np.sin(angles)
+                zs = np.zeros_like(np.array(msg.ranges))
+                points = np.column_stack((xs, ys, zs))
+                pcd = o3d.geometry.PointCloud()
+                pcd.points = o3d.utility.Vector3dVector(points)
+                o3d.io.write_point_cloud(os.path.join(lidar_dir, file_name), pcd)
+                csv_writer.writerow([sec, "", "", "", "", "", "", "", "", os.path.join(lidar_dir, file_name)])
+                lidar_time = timestamp
+
+        elif topic == camera_topic:
+            type = get_message('sensor_msgs/msg/CompressedImage')
+            msg = deserialize_message(data, type)
+            if not camera_time:
+                camera_time = timestamp
+            if int(timestamp.timestamp() - camera_time.timestamp()) >= 1:
+                file_name = "{0}.png".format(sec)
+                np_arr = np.frombuffer(msg.data, np.uint8)
+                image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+                cv2.imwrite(os.path.join(camera_dir, file_name), image_np)
+                csv_writer.writerow([sec, "", "", "", "", "", "", "", os.path.join(camera_dir, file_name), ""])
+                camera_time = timestamp
+
+        elif topic == odometry_topic:
+            type = get_message('nav_msgs/msg/Odometry')
+            msg = deserialize_message(data, type)
+            if not odom_time:
+                odom_time = timestamp
+            if int(timestamp.timestamp() - odom_time.timestamp()) >= 1:
+                if msg.child_frame_id == "base_footprint" and msg.header.frame_id == "odom":
+                    position = msg.pose.pose.position
+                    orientation = msg.pose.pose.orientation
+                    csv_writer.writerow([sec, position.x, position.y, position.z, orientation.w, orientation.x, orientation.y, orientation.z, "", ""])
+                    odom_time = timestamp
+
+    print("Finish to make csv file")
+    print("Start to synchronize csv file")
+    # finish to make csv file
+    import pandas as pd
+
+    df = pd.read_csv(os.path.join(opt.csvDir, 'whole_data.csv'))
+
+    grouped = df.groupby(['time'], as_index=False)
+    result = grouped.first()
+    result_filtered = result.dropna(subset=['time', 'x', 'y', 'z', 'qw', 'qx', 'qy', 'qz', 'image_path', 'lidar_path'])
+    result_filtered.loc[:, ['x', 'y', 'z', 'qw', 'qx', 'qy', 'qz']] = result_filtered.loc[:, ['x', 'y', 'z', 'qw', 'qx', 'qy', 'qz']].apply(lambda x: x.apply(lambda y: f"{float(y):.16f}"))
+
+    result_filtered.to_csv(os.path.join(opt.csvDir, 'whole_synchronized_data.csv'), index=False)
