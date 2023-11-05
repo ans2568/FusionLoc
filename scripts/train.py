50c5f3c38a38744aceb71c14e9d5acff6605a009
ans2568 <ans2568@gmail.com>
2023-11-05 16:56:01 +0900

    argument parser에서 경로에 대한 normalize

--- scripts/train.py
+++ scripts/train.py
@@ -4,7 +4,7 @@ from math import ceil
 import random, shutil, json
 from os import makedirs, remove
 from pathlib import Path
-from os.path import join, exists
+from os.path import join, exists, normpath
 
 import torch
 import torch.nn as nn
@@ -45,11 +45,11 @@ parser.add_argument('--momentum', type=float, default=0.9, help='Momentum for SG
 parser.add_argument('--nocuda', action='store_true', help='Dont use cuda')
 parser.add_argument('--threads', type=int, default=8, help='Number of threads for each data loader to use')
 parser.add_argument('--seed', type=int, default=123, help='Random seed to use.')
-parser.add_argument('--dataPath', type=str, default=join(root, 'dataPath'), help='Path for centroid data.')
-parser.add_argument('--runsPath', type=str, default=join(root, 'runsPath'), help='Path to save runs to.')
-parser.add_argument('--savePath', type=str, default=join(root, 'checkpoints'), 
+parser.add_argument('--dataPath', type=str, default=normpath(join(root, 'dataPath')), help='Path for centroid data.')
+parser.add_argument('--runsPath', type=str, default=normpath(join(root, 'runsPath')), help='Path to save runs to.')
+parser.add_argument('--savePath', type=str, default=normpath(join(root, 'checkpoints')), 
         help='Path to save checkpoints to in logdir. Default=checkpoints/')
-parser.add_argument('--cachePath', type=str, default=join(root, 'cache'), help='Path to save cache to.')
+parser.add_argument('--cachePath', type=str, default=normpath(join(root, 'cache')), help='Path to save cache to.')
 parser.add_argument('--evalEvery', type=int, default=1, 
         help='Do a validation set run, and save, every N epochs.')
 parser.add_argument('--patience', type=int, default=10, help='Patience for early stopping. 0 is off.')
